8bb27527f225f70433666da69b4eeeaca9344010
ThembaNcube <61046678+ThembaNcube@users.noreply.github.com>
2020-11-06 06:36:27 +0200

    Add files via upload

---
+++ Code/BookListMVC/BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
